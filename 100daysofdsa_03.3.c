c8b007d2c8269576cd5631effaeb74b4391df8cc
Tanay Sah <sah_cse@hotmail.com>
2026-02-03 19:13:49 +0530

    Add files via upload

---
+++ 100daysofdsa_03.3.c
@@ -0,0 +1,43 @@
+/*You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.*/
+
+
+
+//tanaysah
+
+include <stdio.h>
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode dummy;
+    dummy.val = 0;
+    dummy.next = NULL;
+
+    struct ListNode *curr = &dummy;
+    int carry = 0;
+
+    while (l1 != NULL || l2 != NULL || carry != 0) {
+        int sum = carry;
+
+        if (l1 != NULL) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+
+        if (l2 != NULL) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+
+        carry = sum / 10;
+
+        struct ListNode *node =
+            (struct ListNode*)malloc(sizeof(struct ListNode));
+        node->val = sum % 10;
+        node->next = NULL;
+
+        curr->next = node;
+        curr = curr->next;
+    }
+
+    return dummy.next;
+}
