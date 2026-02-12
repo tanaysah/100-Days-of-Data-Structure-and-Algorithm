74a213d01570202c2513b156bcf5a20d9eb547b7
Tanay Sah <sah_cse@hotmail.com>
2026-02-12 15:47:40 +0530

    Add files via upload

---
+++ 100daysofdsa_12.2.c
@@ -0,0 +1,21 @@
+
+//Toeplitz Matrix
+
+
+#include <stdbool.h>
+
+bool isToeplitzMatrix(int** matrix, int matrixSize, int* matrixColSize) {
+    
+    int m = matrixSize;
+    int n = matrixColSize[0];
+    
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            if (matrix[i][j] != matrix[i - 1][j - 1]) {
+                return false;
+            }
+        }
+    }
+    
+    return true;
+}
